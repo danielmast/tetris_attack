cec340311fc2c5c1bd8610e70710648603f41f51
danielmast <dmastgithub@gmail.com>
2020-04-25 21:33:47 +0200

    Fix argument usage

--- tetris_attack.py
+++ tetris_attack.py
@@ -18,17 +18,17 @@ def main(argv):
 
 
 def parse_args(argv):
-    if len(argv) < 2:
+    if len(argv) < 3:
         raise Exception('Usage: python tetris_attack.py player_mode bot1 [bot2]')
 
-    player_mode = argv[2]
+    player_mode = argv[1]
     if player_mode != SINGLE_PLAYER and player_mode != VS:
         raise Exception('player_mode should be {} or {}'.format(SINGLE_PLAYER, VS))
 
     bot1 = argv[2]
 
     bot2 = None
-    if (len(argv) == 4):
+    if len(argv) == 4:
         bot2 = argv[3]
 
     return player_mode, bot1, bot2
